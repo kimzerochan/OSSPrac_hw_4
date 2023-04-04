38e855988ee86e4d2bfc81eef9e24231c8991f3f
unknown <qzwx7530@dgu.ac.kr>
2023-04-04 14:56:36 +0900

    ossp1

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 name = input('이름을 입력하세요 : ')
 std_id = int(input('학번을 입력하세요 : '))
 std_st = input('학과를 입력하세요 : ')
-
+sd = input('학교를 입력하세요 : ')
 print('<출력>')
 print('이름 : ',name)
 print('학번 : ',std_id)
 print('학과 : ',std_st)
+print('학교 : ',sd)
